4a386a010aa2e294e3e5388d13b253234560f06b
myszol321 <EeWwAa1>
2022-07-09 21:29:21 +0200

    without css

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,12 +1,14 @@
 import React from "react"
 import pin from "../images/pin.png"
+import photo from "../images/mount-fuji.jpg"
+
 export default function Navbar(props) {
     return (
         <div className="entry">
-            <img src={`../images/${props.coverImg}`}/>
+            <img className="entry--photo" src={photo} alt={"logo"}/>
             <div className="location">
                 <img className="location--pin" src={pin}/>
-                <p>{props.country}</p>
+                <p>{props.country.toUpperCase()}</p>
                 <a href={props.mapsLink}>View on Google Maps</a>
             </div>
             <h1>{props.title}</h1>
